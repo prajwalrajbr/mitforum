e946f3c9c9d3225c6495c46a9b98f4564d4cc5eb
prajwalrajbr <42075029+prajwalrajbr@users.noreply.github.com>
2021-01-30 01:17:40 +0530

    for heroku

--- resources/js/apis/api.js
+++ resources/js/apis/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let api = axios.create({
-    baseURL: "http://localhost:8000/api"
+    baseURL: "https://mitforum.herokuapp.com/api"
 });
 
 api.defaults.withCredentials = true;
